c6a0ea7aabf55872eac43a5157475ab683d64cc4
Doron Tal <mountainbot@gmail.com>
2024-09-30 16:23:40 -0400

    tweaked svelte-demo to create initial website

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+	<title>Blog</title>
+	<meta name="description" content="Blog articles" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>Coming soon</h1>
+</div>
---
+++ src/routes/events/+page.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+	<title>Events</title>
+	<meta name="description" content="Event announcements and calendar" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>Coming soon</h1>
+</div>
