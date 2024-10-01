a3252502f4e2d8e23f7ded0cb06b581f180a593a
Doron Tal <mountainbot@gmail.com>
2024-10-01 10:02:18 -0400

    minor

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,15 +8,34 @@
 </svelte:head>
 
 <section>
-    <img src={photoSrc} alt="Doron Tal" />
+	<h1>Cultivate Mindfulness</h1>
+	<div class="circle-image">
+		<img src={photoSrc} alt="Doron Tal" />
+	</div>
 </section>
 
 <style>
 	section {
 		display: flex;
 		flex-direction: column;
-		justify-content: center;
+		/* justify-content: center; */
 		align-items: center;
 		flex: 0.6;
+	}
+	.circle-image {
+		width: 200px; /* Adjust size as needed */
+		height: 200px;
+		border-radius: 50%;
+		overflow: hidden;
+		border: 2px solid #000; /* Thin border color and width */
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.circle-image img {
+		width: 100%;
+		height: 100%;
+		object-fit: cover; /* Ensures the face is centered */
 	}
 </style>
