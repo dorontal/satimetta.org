fcb10f21a26afa64fceaac9645c3e7f38ed2b8f3
Doron Tal <mountainbot@gmail.com>
2024-10-09 16:21:48 -0400

    Updates

--- _app/immutable/chunks/paths.Qe1nvybu.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_1hp8eym)==null?void 0:s.base)??"/satimetta.org";var e;const a=((e=globalThis.__sveltekit_1hp8eym)==null?void 0:e.assets)??t;export{a,t as b};
--- _app/immutable/entry/start.BokVrdhS.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.B-eX2jIO.js";export{t as start};
