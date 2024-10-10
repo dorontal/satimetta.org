40c3f8699aee152566e9e523608a86371fe9fb13
Doron Tal <mountainbot@gmail.com>
2024-10-10 10:08:35 -0400

    minor

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -4,5 +4,13 @@
 </svelte:head>
 
 <div class="text-column">
-	<h1>Coming soon</h1>
+	<iframe
+	    title="Doron Tal's Mindfulness Events Calendar"
+		src="https://calendar.google.com/calendar/embed?height=550&wkst=1&ctz=America%2FNew_York&bgcolor=%23ffffff&src=YmJkNTRmNzhhNGE2ODNiNGFjYWM4ZjQ2NDJlNmY0YTNkMzkzMTM2MTQyYzkxY2FhODBkNTIxZjRmNzBjYzFiZUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%238E24AA"
+		style="border:solid 1px #777"
+		width="750"
+		height="550"
+		frameborder="0"
+		scrolling="no"
+	></iframe>
 </div>
