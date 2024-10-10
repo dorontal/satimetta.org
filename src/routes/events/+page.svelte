f50819bc1ff1fab93f95f2dd85f96138fa5522ea
Doron Tal <mountainbot@gmail.com>
2024-10-10 10:38:39 -0400

    minor

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -6,7 +6,7 @@
 <div class="calendar">
 	<iframe
 	    title="ddd"
-		src="https://calendar.google.com/calendar/embed?height=550&wkst=1&ctz=America%2FNew_York&bgcolor=%23ffffff&showPrint=0&showTabs=0&showCalendars=0&title&showTitle=0&src=YmJkNTRmNzhhNGE2ODNiNGFjYWM4ZjQ2NDJlNmY0YTNkMzkzMTM2MTQyYzkxY2FhODBkNTIxZjRmNzBjYzFiZUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%238E24AA"
+		src="https://calendar.google.com/calendar/embed?height=320&wkst=1&ctz=America%2FNew_York&bgcolor=%23ffffff&showPrint=0&showTabs=0&showCalendars=0&title&showTitle=0&src=YmJkNTRmNzhhNGE2ODNiNGFjYWM4ZjQ2NDJlNmY0YTNkMzkzMTM2MTQyYzkxY2FhODBkNTIxZjRmNzBjYzFiZUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%238E24AA"
 		style="border-width:0"
 		width="320"
 		height="320"
