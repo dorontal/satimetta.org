a0e1db0e0e919c9df7d37c0aec61022876807270
Doron Tal <mountainbot@gmail.com>
2024-10-10 10:34:18 -0400

    adde embedded google calendar

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -3,14 +3,15 @@
 	<meta name="description" content="Event announcements and calendar" />
 </svelte:head>
 
-<div class="text-column">
+<div class="calendar">
 	<iframe
-	    title="Doron Tal's Mindfulness Events Calendar"
-		src="https://calendar.google.com/calendar/embed?height=550&wkst=1&ctz=America%2FNew_York&bgcolor=%23ffffff&src=YmJkNTRmNzhhNGE2ODNiNGFjYWM4ZjQ2NDJlNmY0YTNkMzkzMTM2MTQyYzkxY2FhODBkNTIxZjRmNzBjYzFiZUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%238E24AA"
-		style="border:solid 1px #777"
-		width="750"
-		height="550"
+	    title="ddd"
+		src="https://calendar.google.com/calendar/embed?height=550&wkst=1&ctz=America%2FNew_York&bgcolor=%23ffffff&showPrint=0&showTabs=0&showCalendars=0&title&showTitle=0&src=YmJkNTRmNzhhNGE2ODNiNGFjYWM4ZjQ2NDJlNmY0YTNkMzkzMTM2MTQyYzkxY2FhODBkNTIxZjRmNzBjYzFiZUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%238E24AA"
+		style="border-width:0"
+		width="320"
+		height="320"
 		frameborder="0"
 		scrolling="no"
 	></iframe>
 </div>
+
