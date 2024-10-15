c33c041d12cdf939eae7da75f3994ebbd75ba201
Doron Tal <mountainbot@gmail.com>
2024-10-15 13:26:00 -0400

    minor

---
+++ src/routes/resources/four-foundations-of-mindfulness/+page.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+	<title>Four Foundations of Mindfulness</title>
+	<meta name="description" content="Four Foundations of Mindfulness" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>Coming soon</h1>		
+</div>
