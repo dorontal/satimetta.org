539ef80d7671063dee1ab13d647310bdced2bbb8
Doron Tal <mountainbot@gmail.com>
2024-11-03 19:53:12 -0500

    minor

--- src/routes/resources/bio/+page.svelte
+++ src/routes/resources/bio/+page.svelte
@@ -8,67 +8,36 @@
 </div>
 
 <!--div class="text-column">
-	<h1>Practice History</h1>
+	<h1>Practice and Training History</h1>
 	<ul>
 		<li>
-			<h4>Mid 1980's</h4>
+			<h4>Mid 1980's - Present - Breathwork and Body Scan</h4>
 			<ul>
 				<li>
-					<p>
-						Started with a yoga breathing exercise for calm, combining deep abdominal long breathing
-						with visualization.
-					</p>
+					Started breath work: for years it was just one yoga breathing exercise for calm: abdominal
+					long breathing using counting and visualization.
 				</li>
+				<li>Started Body scan practice.</li>
 				<li>
-					<p>
-						Dabbled in self-hypnosis, after having read a book on it with exercises, including a
-						body scan.
-					</p>
-				</li>
-				<li>
-					<p>
-						An interest in the mind is an important component of mindfulness practice. Mindfulness
-						is always mindfulness of something, where 'something' is one of four categories of which
-						'mind' is one. Mindfulness of mind - awareness of one's mental processes - is one of the <a
-							href="/resources/four-foundations-of-mindfulness">Four Foundations of Mindfulness</a
-						>. Practicing the four foundations enables further insights about the mind. My interest
-						in studying the mind began, partially due to a philosophy class that covered Descartes'
-						Meditations, and also due to a biology teacher and mentor, Brian Kahn, who convinced me
-						that the brain is the most pressing enigma for humanity to uncover and inspired me to
-						study neuroscience.
-					</p>
+					An interest in the mind started in a high school philosophy class that covered Descartes;
+					also the biology teacher, Brian Kahn, inspired me by saying that the brain is the most
+					pressing enigma left for science to uncover.
 				</li>
 			</ul>
+		</li>
+		<li>
+			<h4>1988 - Present - Tai Chi</h4>
 			<p>
-				Yoga breathing exercise for calm, combining deep abdominal long breathing with
-				visualization.
-			</p>
-			<p>
-				Dabbled in self-hypnosis, after having read a book on it with exercises, including a body
-				scan.
-			</p>
-			<p>
-				An interest in the mind is an important component of mindfulness practice. Mindfulness is
-				always mindfulness of something, where 'something' is one of four categories of which 'mind'
-				is one. Mindfulness of mind - awareness of one's mental processes - is one of the <a
-					href="/resources/four-foundations-of-mindfulness">Four Foundations of Mindfulness</a
-				>. Practicing the four foundations enables further insights about the mind. My interest in
-				studying the mind began, partially due to a philosophy class that covered Descartes'
-				Meditations, and also due to a biology teacher and mentor, Brian Kahn, who convinced me that
-				the brain is the most pressing enigma for humanity to uncover and inspired me to study
-				neuroscience.
+				Tai Chi: specializing in Yang Style Short Form of Prof. Tseng Man Ts'ing. Studied with
+				Katherine Hannah, Maggie Newman, Sifu William Dere, and since the late 80's with William C.
+				C. Chen.
 			</p>
 		</li>
 		<li>
-			<h4>Late 1980's</h4>
+			<h4>1989 - Present - Qigong</h4>
 			<p>
-				Started Tai Chi in 1988, studying with Katherine Hannah, who was Ben Lowe's student, who was
-				Prof. Cheng Man Ch'ing's student. I continue doing the Yang Style Short Form twice daily
-				until today. I later studied with Maggie Newman, Sifu William Dere, and since the late 80's
-				with William C. C. Chen. In 1989, I learned a Chi Kung (Qigong) set from Robert Tangora that
-				I still practice and which I have taught a few times. This Chi Kung set was designed as a
-				prescription to stretch the spine. As part of Tai Chi, learned a bit more about Taoism, by
-				reading Lao Tzu's Tao Te Ching.
+				Learned a Qigong (Chi Kung) set for stretching and healing the spine, taught by Robert
+				Tangora.
 			</p>
 		</li>
 		<li>
@@ -81,9 +50,19 @@
 				grudgingly, but it ended up being life transforming. The talk was titled "Overcoming Differences".
 				The main message was that anger is self-damaging, that is is never is useful, and that we are
 				the masters of our own anger - we always have the choice to not engage with anger. This talk
-				instantly helped me become a safer driver as it causd a great reduction of driving-related anger.
-				After his talk I got his book "The Art of Happiness", which started me on a path of Metta long
-				before awareness of breath meditation.
+				caused me to become a safer driver as this was the first area of application. After his talk
+				I got his book "The Art of Happiness", which started me on a path of Metta long before awareness
+				of breath meditation practice, though it was not formal Metta practice yet.
+			</p>
+		</li>
+		<li>
+			<h4>1997 - Neuroscience Ph. D.</h4>
+			<p>
+				This is somewhat related to mindfulness practice because today researchers try to explain
+				the benefits of mindfulness practice, e.g. cases of neuro-prasticity or neurogenesis, via
+				neuroscientific observations, mostly related to various parts of the brain. While I prefer
+				the psychology narrative, which goes all the way back to the time of the Buddha, I try to
+				keep up a bit with the neuroscience narrative, due to its popularity.
 			</p>
 		</li>
 		<li>
@@ -94,42 +73,49 @@
 			</p>
 		</li>
 		<li>
-			<h4>2017 - Started with Bhikkhu Bodhi's teachings</h4>
+			<h4>2015 - Present - Metta Practice</h4>
 			<p>
-				Before the pandemic, I attended a couple of sutta class lectures by Bhikkhu Bodhi online.
-				During the pandemic, and ever since, I've been attending both his Wednesday night Metta
-				meditation and chanting session and the Saturday morning sutta class and meditation sessions
-				regularly.
+				Before the pandemic, I dabbled in Metta practice at Tibet House in New York City, under the
+				instruction of Rachel Hammerman. Since 2019, I've attended Bhikkhu Bodhi's weekly Wednesday
+				hour of Metta practice until today.
 			</p>
 		</li>
-
+		<li>
+			<h4>2017 - Started with Bhikkhu Bodhi's Sutta Study class</h4>
+			<p>
+				Before the pandemic, I dabbled in sutta class lectures by Bhikkhu Bodhi online. During the
+				pandemic, and ever since, I've been attending the Saturday morning sutta class regularly.
+			</p>
+		</li>
 		<li>
 			<h4>2019 - First Meditation Retreat - Phuket, Thailand</h4>
 			<p>
 				Attended a retreat taught by Tobi Warzinek at "Dharana Meditation" in Phuket ("Phuket
-				Meditation Center" today). Tobi used a method similar to Ajahn Brahm's. After this retreat,
-				I read "Mindfulness, Bliss and Beyond" by Ajahn Brahm and continued daily practice with that
-				method for a couple of years.
+				Meditation Center" today). Tobi used a meditation method similar to Ajahn Brahm's. After
+				this retreat, I read "Mindfulness, Bliss and Beyond" by Ajahn Brahm and continued daily
+				practice with that method for a couple of years.
 			</p>
 		</li>
 		<li>
 			<h4>
-				2019 - Started Walking Meditation
+				2019 - Present - Started Walking Meditation
 				<p>
-					The method I used was baseed on a Theravada method explaine in <a
+					The method I first used was baseed on a Theravada method explaine in <a
 						href="https://www.youtube.com/watch?v=_IFvablc6EI">this video</a
 					> by Yuttadhammo Bhikkhu. I have continued to practice walking meditation and choose from either
-					this method or two other ones, depending on circumstances.
+					this method or two other further refined methods, depending on circumstances.
 				</p>
 			</h4>
 		</li>
 		<li>
 			<h4>
-				2019 - 2020 - Practiced AJahn Brahm's Method in "Mindfulness, Bliss and Beyond"
+				2019 - 2020 - Practiced AJahn Brahm's Meditation Method"
 				<p>
-					Following the retreat in Thailand, which was based on Ajahn Brahm's method as outlined in
-					his book "Mindfulness, Bliss and Beyond", I read the book and began training in this
-					method. There are elements from this method that I use to this day, but
+					Following the 2019 retreat in Thailand, which was based on Ajahn Brahm's method as
+					outlined in his book "Mindfulness, Bliss and Beyond", I followed this method according to
+					the book for about two years. There are elements from this method that I use to this day,
+					but I later switched to practicing the Buddha's 16-step method as described in the
+					Ānāpānasati sutta MN 118.
 				</p>
 			</h4>
 		</li>
@@ -137,13 +123,24 @@
 			<h4>
 				2021 - Present - Practiced the Buddha's Method of In/Out Breath Meditation
 				<p>
-					After carefully studying two suttas in the Middle Length Discourses of the Pali Canon -
-					the Satipatthana Sutta (MN 10) and the Anapanasati Sutta (MN 118) - I began practicing the
+					After studying two suttas in the Middle Length Discourses of the Pali Canon - the
+					Satipatthana Sutta (MN 10) and the Ānāpānasati Sutta (MN 118) - I began practicing the
 					sixteen step meditation instructions of the Buddha. These steps are a bit vague at places
 					and require interpretation. I have read many people's interpretations of the sixteen steps
 					and this helped to eventually form my own interpretation.
 				</p>
 			</h4>
+		</li>
+		<li>
+			<h4>2023-03-17 - 4-Day Virtual Silent Retreat with Bhikkhu Bodhi</h4>
+			<p>
+				Virtual online retreat, titled
+				<a
+					href="https://4nqqe.r.ag.d.sendibm3.com/mk/mr/mSFedAVBuMUCXhE_LqNU_CdoRSZwIcHpQ8ppyKnwbUClA8-HbYKyIeZ1rpfTQrx7yV4YAkolm1DECzKj6Su_KjvXLSxAca6SDDWwtdybmcNqN_OPvmTTbYVtnLhWFK8sH0c5MZIidQ"
+				>
+					From Contemplation of the Body to Contemplation of the Mind
+				</a>.
+			</p>
 		</li>
 	</ul>
 </div-->
