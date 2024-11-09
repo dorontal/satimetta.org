6dde498196070499c37f756f92d93dd87ef661a5
Doron Tal <mountainbot@gmail.com>
2024-11-09 15:46:17 -0500

    deployed, 1st one w/bio

--- src/routes/resources/bio/+page.svelte
+++ src/routes/resources/bio/+page.svelte
@@ -3,144 +3,165 @@
 	<meta name="description" content="About Doron Tal" />
 </svelte:head>
 
-<div class="text-column">
-	<h1>Under Construction</h1>
-</div>
-
 <!--div class="text-column">
+	<h1>Under Construction</h1>
+</div-->
+
+<div class="text-column">
 	<h1>Practice and Training History</h1>
 	<ul>
 		<li>
-			<h4>Mid 1980's - Present - Breathwork and Body Scan</h4>
-			<ul>
-				<li>
-					Started breath work: for years it was just one yoga breathing exercise for calm: abdominal
-					long breathing using counting and visualization.
-				</li>
-				<li>Started Body scan practice.</li>
-				<li>
-					An interest in the mind started in a high school philosophy class that covered Descartes;
-					also the biology teacher, Brian Kahn, inspired me by saying that the brain is the most
-					pressing enigma left for science to uncover.
-				</li>
-			</ul>
-		</li>
-		<li>
-			<h4>1988 - Present - Tai Chi</h4>
+			<h4>Engaged Mindfulness Institute - Mindfulness Teacher Training (May 2024 - Present)</h4>
 			<p>
-				Tai Chi: specializing in Yang Style Short Form of Prof. Tseng Man Ts'ing. Studied with
-				Katherine Hannah, Maggie Newman, Sifu William Dere, and since the late 80's with William C.
-				C. Chen.
+				The <a href="https://www.engagedmindfulness.org/">Engaged Mindfulness Institute</a>
+				(EMI) trains its students to facilitate mindfulness instruction in trauma-informed manner. The
+				EMI mindfulness teacher training certifies its graduates via the
+				<a href="https://imta.org">International Mindfulness Teacher Association</a>'s accreditation
+				as a Certified Mindfulness Teaching Professional (CMT-P). I have completed most of the
+				requirements and hope to be certified by May, 2025.
 			</p>
 		</li>
 		<li>
-			<h4>1989 - Present - Qigong</h4>
+			<h4>MBSR 10-Week Teacher Training Foundations Class (2024)</h4>
 			<p>
-				Learned a Qigong (Chi Kung) set for stretching and healing the spine, taught by Robert
-				Tangora.
+				Attended the 10-week MBSR Teacher Training Class offered by UCSD, taught by Noriko Morita
+				Harth and Korantema Pierce Wiliams.
 			</p>
 		</li>
 		<li>
-			<h4>1991 - A talk by His Holiness the Dalai Lama</h4>
+			<h4>Santikaro's Class: Ānāpānasati Conversations (2024)</h4>
 			<p>
-				In 1991, His Holiness the Dalai Lama came to visit the U.S. for the first time in many
-				years. He
-				<a href="https://www.youtube.com/watch?v=_2G-4m4y8Ss">gave his first talk</a> at the university
-				where I was studying. I didn't even know who he was. My roommate dragged me to the talk. I went,
-				grudgingly, but it ended up being life transforming. The talk was titled "Overcoming Differences".
-				The main message was that anger is self-damaging, that is is never is useful, and that we are
-				the masters of our own anger - we always have the choice to not engage with anger. This talk
-				caused me to become a safer driver as this was the first area of application. After his talk
-				I got his book "The Art of Happiness", which started me on a path of Metta long before awareness
-				of breath meditation practice, though it was not formal Metta practice yet.
+				A year long class centered around the Buddha's 16 step meditation method.	
 			</p>
 		</li>
 		<li>
-			<h4>1997 - Neuroscience Ph. D.</h4>
+			<h4>Santikaro's Class: Monday Morning Guided Ānāpānasati Meditations (2024)</h4>
 			<p>
-				This is somewhat related to mindfulness practice because today researchers try to explain
-				the benefits of mindfulness practice, e.g. cases of neuro-prasticity or neurogenesis, via
-				neuroscientific observations, mostly related to various parts of the brain. While I prefer
-				the psychology narrative, which goes all the way back to the time of the Buddha, I try to
-				keep up a bit with the neuroscience narrative, due to its popularity.
+				A year long class where Santikaro guides Ānāpānasati meditations.	
 			</p>
 		</li>
 		<li>
-			<h4>2005 - Taught Chi Kung at a gym class</h4>
+			<h4>Santikaro Retreat (January 2024)</h4>
 			<p>
-				After the Tai Chi teacher at a gym in my workplace left, I ended up teaching one semester of
-				the Chi Kung set I had learned in the late 1980s to coworkers.
+				Virtual online retreat titled
+				<a href="https://www.kevalaretreat.org/santikaros-2024-on-line-retreat-schedule/"
+					>Mindfulness with Breathing: Contemplating Citta (mind, heart, psyche)</a
+				>, with <a href="https://www.kevalaretreat.org/who-we-are/">Santikaro</a>.
 			</p>
 		</li>
 		<li>
-			<h4>2015 - Present - Metta Practice</h4>
+			<h4>Santikaro's Class: Ānāpānasati Conversations (2023)</h4>
 			<p>
-				Before the pandemic, I dabbled in Metta practice at Tibet House in New York City, under the
-				instruction of Rachel Hammerman. Since 2019, I've attended Bhikkhu Bodhi's weekly Wednesday
-				hour of Metta practice until today.
+				A year long class centered around the Buddha's 16 step meditation method.	
 			</p>
 		</li>
 		<li>
-			<h4>2017 - Started with Bhikkhu Bodhi's Sutta Study class</h4>
+			<h4>MBSR Class (September 2023)</h4>
 			<p>
-				Before the pandemic, I dabbled in sutta class lectures by Bhikkhu Bodhi online. During the
-				pandemic, and ever since, I've been attending the Saturday morning sutta class regularly.
+				Attended an MBSR class, in exploration of teaching MBSR later. This was the UCSD class taugt
+				by Megan Prager and Jennifer Miller.
 			</p>
 		</li>
 		<li>
-			<h4>2019 - First Meditation Retreat - Phuket, Thailand</h4>
+			<h4>4-Day Virtual Silent Retreat with Bhikkhu Bodhi (March, 2023)</h4>
 			<p>
-				Attended a retreat taught by Tobi Warzinek at "Dharana Meditation" in Phuket ("Phuket
-				Meditation Center" today). Tobi used a meditation method similar to Ajahn Brahm's. After
-				this retreat, I read "Mindfulness, Bliss and Beyond" by Ajahn Brahm and continued daily
-				practice with that method for a couple of years.
-			</p>
-		</li>
-		<li>
-			<h4>
-				2019 - Present - Started Walking Meditation
-				<p>
-					The method I first used was baseed on a Theravada method explaine in <a
-						href="https://www.youtube.com/watch?v=_IFvablc6EI">this video</a
-					> by Yuttadhammo Bhikkhu. I have continued to practice walking meditation and choose from either
-					this method or two other further refined methods, depending on circumstances.
-				</p>
-			</h4>
-		</li>
-		<li>
-			<h4>
-				2019 - 2020 - Practiced AJahn Brahm's Meditation Method"
-				<p>
-					Following the 2019 retreat in Thailand, which was based on Ajahn Brahm's method as
-					outlined in his book "Mindfulness, Bliss and Beyond", I followed this method according to
-					the book for about two years. There are elements from this method that I use to this day,
-					but I later switched to practicing the Buddha's 16-step method as described in the
-					Ānāpānasati sutta MN 118.
-				</p>
-			</h4>
-		</li>
-		<li>
-			<h4>
-				2021 - Present - Practiced the Buddha's Method of In/Out Breath Meditation
-				<p>
-					After studying two suttas in the Middle Length Discourses of the Pali Canon - the
-					Satipatthana Sutta (MN 10) and the Ānāpānasati Sutta (MN 118) - I began practicing the
-					sixteen step meditation instructions of the Buddha. These steps are a bit vague at places
-					and require interpretation. I have read many people's interpretations of the sixteen steps
-					and this helped to eventually form my own interpretation.
-				</p>
-			</h4>
-		</li>
-		<li>
-			<h4>2023-03-17 - 4-Day Virtual Silent Retreat with Bhikkhu Bodhi</h4>
-			<p>
-				Virtual online retreat, titled
+				Online retreat titled
 				<a
 					href="https://4nqqe.r.ag.d.sendibm3.com/mk/mr/mSFedAVBuMUCXhE_LqNU_CdoRSZwIcHpQ8ppyKnwbUClA8-HbYKyIeZ1rpfTQrx7yV4YAkolm1DECzKj6Su_KjvXLSxAca6SDDWwtdybmcNqN_OPvmTTbYVtnLhWFK8sH0c5MZIidQ"
-				>
-					From Contemplation of the Body to Contemplation of the Mind
-				</a>.
+					>From Contemplation of the Body to Contemplation of the Mind</a
+				>.
+			</p>
+		</li>
+		<li>
+			<h4>Ānāpānasati Meditation Method (2020 - Present)</h4>
+			<p>
+				Practice of the sixteen-step meditation instructions of the Buddha. These steps are somewhat
+				open to interpretation - multiple perspectives have helped form an approach and
+				interpretation of these instructions.
+			</p>
+		</li>
+		<li>
+			<h4>Ajahn Brahm's Meditation Method (2019 - 2020)</h4>
+			<p>
+				Following a 2019 retreat in Thailand based on Ajahn Brahm's approach from his book
+				"Mindfulness, Bliss and Beyond," I practiced his method for about two years. I now
+				incorporate some elements from it, though I currently follow the Buddha's 16-step method as
+				described in the Ānāpānasati Sutta MN 118.
+			</p>
+		</li>
+		<li>
+			<h4>Walking Meditation (2019 - Present)</h4>
+			<p>
+				Started with a Theravada walking meditation method described in
+				<a href="https://www.youtube.com/watch?v=_IFvablc6EI">this video by Yuttadhammo Bhikkhu</a>.
+				Later, I incorporated two more refined techniques, for more choice in different contexts.
+			</p>
+		</li>
+		<li>
+			<h4>Meditation Retreat with Toby Warzinek (2019)</h4>
+			<p>
+				Retreat led by Tobi Warzinek at the <a href="https://phuket-meditation.com/"
+					>Phuket Meditation Center</a
+				>. He taught a meditation approach similar to Ajahn Brahm’s. After the retreat, I picked up
+				"Mindfulness, Bliss and Beyond" by Ajahn Brahm and practiced daily with that method for
+				several years.
+			</p>
+		</li>
+		<li>
+			<h4>Sutta Study Class (2017 - Present)</h4>
+			<p>Regular attendance and participation in the Saturday class and meditation sessions.</p>
+		</li>
+		<li>
+			<h4>Metta Practice (2015 - Present)</h4>
+			<p>
+				Practicing Metta at Tibet House in New York City under the guidance of <a
+					href="https://www.rachelhammerman.com/">Rachel Hammerman</a
+				>. Since 2019, I’ve joined
+				<a href="https://www.baus.org/en/group-meditation-over-zoom/"
+					>Bhikkhu Bodhi's weekly Metta
+				</a>
+			</p>
+		</li>
+		<li>
+			<h4>Teaching Qigong (2005)</h4>
+			<p>After the Tai Chi teacher at our workplace gym left, I taught a Qigong class.</p>
+		</li>
+		<li>
+			<h4>Neuroscience Ph.D. (1997)</h4>
+			<p>
+				Indirectly related - allows an understanding of the modern narratives that try to explain
+				how mindfulness works from a science perspective.
+			</p>
+		</li>
+		<li>
+			<h4>A Talk by His Holiness the Dalai Lama (1991)</h4>
+			<p>
+				In 1991, His Holiness the Dalai Lama gave a transformative talk titled <a
+					href="https://www.youtube.com/watch?v=_2G-4m4y8Ss">"Overcoming Differences"</a
+				>. This experience inspired me to begin reading his work.
+			</p>
+		</li>
+		<li>
+			<h4>Qigong (1989 - Present)</h4>
+			<p>
+				Learned a Qigong (Chi Kung) set from Robert Tangora, focusing on spinal stretching and
+				healing.
+			</p>
+		</li>
+		<li>
+			<h4>Tai Chi (1988 - Present)</h4>
+			<p>
+				Tai Chi is also moving meditation. Practicing mainly the Yang Style Short Form of Prof.
+				Tseng Man Ts'ing, under the instruction of Katherine Hannah, Maggie Newman, William Dere,
+				and since the late 80's, William C. C. Chen.
+			</p>
+		</li>
+		<li>
+			<h4>Breathwork & Body Scans (Mid 1980's - Present)</h4>
+			<p>
+				Practicing yoga breathing exercises to calm the body, using abdominal breathing, counting,
+				and visualization. I also began practicing body scan techniques.
 			</p>
 		</li>
 	</ul>
-</div-->
+</div>
