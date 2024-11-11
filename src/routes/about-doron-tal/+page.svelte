515c3e5d4a2bb4d7cd3d8fc9a9a331849cdadc16
Doron Tal <mountainbot@gmail.com>
2024-11-10 22:48:05 -0500

    deployed

--- src/routes/about-doron-tal/+page.svelte
+++ src/routes/about-doron-tal/+page.svelte
@@ -136,7 +136,8 @@
 			<p>
 				In 1991, His Holiness the Dalai Lama gave a transformative talk titled <a
 					href="https://www.youtube.com/watch?v=_2G-4m4y8Ss">"Overcoming Differences"</a
-				>. This experience inspired me to begin reading his work, mostly related to Metta.
+				>. This experience inspired me to read "The Art of Happiness" and to try and follow its
+				advice.
 			</p>
 		</li>
 		<li>
