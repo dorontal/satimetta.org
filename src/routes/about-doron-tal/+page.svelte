55a1b5d7cfd20c318d11d68c8ad37718a08dade9
Doron Tal <mountainbot@gmail.com>
2024-11-30 21:57:23 -0500

    minor

--- src/routes/about-doron-tal/+page.svelte
+++ src/routes/about-doron-tal/+page.svelte
@@ -12,8 +12,9 @@
 				<li>
 					<h4>Daily Sitting Meditation Practice Group (2023 - Present)</h4>
 					<p>
-						Daily online group meditations with <a href="https://www.clearmountainmonastery.org/"
-							>Clear Mountain Monastery</a
+						Daily online group meditations with <a
+							href="https://www.clearmountainmonastery.org/"
+							target="_blank">Clear Mountain Monastery</a
 						>.
 					</p>
 				</li>
@@ -32,7 +33,8 @@
 					<h4>Walking Meditation (2019 - Present)</h4>
 					<p>
 						Began with Yuttadhammo Bhikkhu's Theravada <a
-							href="https://www.youtube.com/watch?v=_IFvablc6EI">walking meditation instructions</a
+							href="https://www.youtube.com/watch?v=_IFvablc6EI"
+							target="_blank">walking meditation instructions</a
 						> (one note per step), added Bhikkhu Bodhi's three-point technique (picking, placing, shifting)
 						and a six-point method. The approach varies depending on mental state at the start of each
 						session.
@@ -42,9 +44,10 @@
 					<h4>Metta Practice (2015 - Present)</h4>
 					<p>
 						Practiced Metta at Tibet House in New York City under the guidance of <a
-							href="https://www.rachelhammerman.com/">Rachel Hammerman</a
+							href="https://www.rachelhammerman.com/"
+							target="_blank">Rachel Hammerman</a
 						>. Regularly attending
-						<a href="https://www.baus.org/en/group-meditation-over-zoom/"
+						<a href="https://www.baus.org/en/group-meditation-over-zoom/" target="_blank"
 							>Bhikkhu Bodhi's weekly Metta
 						</a> sessions since 2019.
 					</p>
@@ -55,6 +58,7 @@
 						Learned a Qigong (Chi Kung) set for spine healing, strengthening and stretching, created
 						by Master Jiao Guo Ryu, from <a
 							href="https://www.eomega.org/workshops/teachers/robert-tangora"
+							target="_blank"
 						>
 							Robert Tangora</a
 						>.
@@ -64,16 +68,18 @@
 					<h4>Tai Chi (1988 - Present)</h4>
 					<p>
 						Tai Chi is moving meditation. I mainly practice the
-						<a href="https://www.youtube.com/watch?v=3_BKwlpOAkk">
+						<a href="https://www.youtube.com/watch?v=3_BKwlpOAkk" target="_blank">
 							Yang Style Short Form of Prof. Chen Man Ching</a
 						>
 						and have studied under the instruction of Katherine Hannah,
-						<a href="https://www.youtube.com/watch?v=6rLG3Ys-wIU">Maggie Newman</a>,
+						<a href="https://www.youtube.com/watch?v=6rLG3Ys-wIU" target="_blank">Maggie Newman</a>,
 						<a
 							href="https://www.facebook.com/UCBTaiji/videos/william-dere-pushing-hands-principles/2454512608101776/"
-							>Sifu William Dere</a
+							target="_blank">Sifu William Dere</a
 						>, and since the late 80's, with
-						<a href="https://www.williamccchen.com/">Great Grandmaster William C. C. Chen</a>.
+						<a href="https://www.williamccchen.com/" target="_blank"
+							>Great Grandmaster William C. C. Chen</a
+						>.
 					</p>
 				</li>
 				<li>
@@ -118,12 +124,15 @@
 				<li>
 					<h4>Engaged Mindfulness Institute - Mindfulness Teacher Training (May 2024 - Present)</h4>
 					<p>
-						The <a href="https://www.engagedmindfulness.org/">Engaged Mindfulness Institute</a>
+						The <a href="https://www.engagedmindfulness.org/" target="_blank"
+							>Engaged Mindfulness Institute</a
+						>
 						trains its students to facilitate mindfulness instruction in trauma-informed manner. The
 						EMI mindfulness teacher training certifies its graduates via the
-						<a href="https://imta.org">International Mindfulness Teacher Association</a>'s
-						accreditation as a Certified Mindfulness Teaching Professional (CMT-P). I have completed
-						most of the requirements and hope to be certified by May, 2025.
+						<a href="https://imta.org" target="_blank"
+							>International Mindfulness Teacher Association</a
+						>'s accreditation as a Certified Mindfulness Teaching Professional (CMT-P). I have
+						completed most of the requirements and hope to be certified by May, 2025.
 					</p>
 				</li>
 				<li>
@@ -142,14 +151,16 @@
 					<h4>MBSR 10-Week Teacher Training Foundations Class (2024)</h4>
 					<p>
 						Attended the 10-week MBSR Teacher Training class offered by University of California,
-						San Diego (UCSD)'s <a href="https://cih.ucsd.edu/">Centers for Integrative Health</a>,
-						taught by
-						<a href="https://cih.ucsd.edu/mindfulness/team/noriko-morita-harth"
+						San Diego (UCSD)'s <a href="https://cih.ucsd.edu/" target="_blank"
+							>Centers for Integrative Health</a
+						>, taught by
+						<a href="https://cih.ucsd.edu/mindfulness/team/noriko-morita-harth" target="_blank"
 							>Noriko Morita Harth</a
 						>
 						and
-						<a href="https://www.mindful-way.com/teachers/korantemah-pierce-williams/"
-							>Korantema Pierce Wiliams</a
+						<a
+							href="https://www.mindful-way.com/teachers/korantemah-pierce-williams/"
+							target="_blank">Korantema Pierce Wiliams</a
 						>.
 					</p>
 				</li>
@@ -157,7 +168,8 @@
 					<h4>Santikaro's Class: Monday Morning Guided Ānāpānasati Meditations (2024)</h4>
 					<p>
 						A year long class where Santikaro guides Ānāpānasati meditations, offered online via <a
-							href="https://kevalaretreat.org">Kevala Retreat</a
+							href="https://kevalaretreat.org"
+							target="_blank">Kevala Retreat</a
 						>.
 					</p>
 				</li>
@@ -165,20 +177,22 @@
 					<h4>Santikaro's Class: Ānāpānasati Conversations (2023-2024)</h4>
 					<p>
 						A year long class centered around the Buddha's 16 step meditation method, offered online
-						via <a href="https://kevalaretreat.org">Kevala Retreat</a>.
+						via <a href="https://kevalaretreat.org" target="_blank">Kevala Retreat</a>.
 					</p>
 				</li>
 				<li>
 					<h4>MBSR Class (September 2023)</h4>
 					<p>
 						Attended an MBSR class, in exploration of teaching MBSR later. This was a University of
-						California, San Diego (UCSD)'s <a href="https://cih.ucsd.edu/"
+						California, San Diego (UCSD)'s <a href="https://cih.ucsd.edu/" target="_blank"
 							>Centers for Integrative Health</a
 						>
 						class taugt by
-						<a href="https://cih.ucsd.edu/mindfulness/team/megan-prager-ma">Megan Prager</a>
+						<a href="https://cih.ucsd.edu/mindfulness/team/megan-prager-ma" target="_blank"
+							>Megan Prager</a
+						>
 						and
-						<a href="https://cih.ucsd.edu/mindfulness/team/jennifer-miller-psyd"
+						<a href="https://cih.ucsd.edu/mindfulness/team/jennifer-miller-psyd" target="_blank"
 							>Jennifer A. Miller</a
 						>.
 					</p>
@@ -187,7 +201,8 @@
 					<h4>A Talk by His Holiness the Dalai Lama (1991)</h4>
 					<p>
 						Attended a talk by his Holiness the Dalai Lama, titled <a
-							href="https://www.youtube.com/watch?v=_2G-4m4y8Ss">"Overcoming Differences"</a
+							href="https://www.youtube.com/watch?v=_2G-4m4y8Ss"
+							target="_blank">"Overcoming Differences"</a
 						>. This catalyzed further study of his work and the Buddha's path.
 					</p>
 				</li>
@@ -225,9 +240,12 @@
 					<h4>Retreat with Santikaro (January 21-27, 2024)</h4>
 					<p>
 						Virtual online retreat titled
-						<a href="https://www.kevalaretreat.org/santikaros-2024-on-line-retreat-schedule/"
+						<a
+							href="https://www.kevalaretreat.org/santikaros-2024-on-line-retreat-schedule/"
+							target="_blank"
 							>Mindfulness with Breathing: Contemplating Citta (mind, heart, psyche)</a
-						>, with <a href="https://www.kevalaretreat.org/who-we-are/">Santikaro</a>.
+						>, with
+						<a href="https://www.kevalaretreat.org/who-we-are/" target="_blank">Santikaro</a>.
 					</p>
 				</li>
 				<li>
@@ -236,14 +254,15 @@
 						Participated and volunteered in this online silent retreat, titled
 						<a
 							href="https://4nqqe.r.ag.d.sendibm3.com/mk/mr/mSFedAVBuMUCXhE_LqNU_CdoRSZwIcHpQ8ppyKnwbUClA8-HbYKyIeZ1rpfTQrx7yV4YAkolm1DECzKj6Su_KjvXLSxAca6SDDWwtdybmcNqN_OPvmTTbYVtnLhWFK8sH0c5MZIidQ"
-							>From Contemplation of the Body to Contemplation of the Mind</a
+							target="_blank">From Contemplation of the Body to Contemplation of the Mind</a
 						>.
 					</p>
 				</li>
 				<li>
 					<h4>Retreat with Tobi Warzinek (March 5-9, 2019)</h4>
 					<p>
-						Five day retreat at <a href="https://phuket-meditation.com/">Phuket Meditation Center</a
+						Five day retreat at <a href="https://phuket-meditation.com/" target="_blank"
+							>Phuket Meditation Center</a
 						>. Tobi taught a meditation method based on Ajahn Brahm’s. After the retreat, I picked
 						up "Mindfulness, Bliss and Beyond" by Ajahn Brahm and continued daily practice with that
 						method for a few years.
