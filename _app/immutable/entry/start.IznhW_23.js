256f54a911107d702f08622c3b903d9848f4e430
dorontal <mountainbot@gmail.com>
2025-03-15 21:26:35 -0400

    Updates

--- _app/immutable/entry/start.IznhW_23.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.djvFqurl.js";export{t as start};
--- _app/immutable/nodes/4.B0zZRHOq.js
+++ _app/immutable/nodes/4.B0zZRHOq.js
@@ -87,7 +87,7 @@ import{f as A,a as n,t as s,g as r}from"../chunks/disclose-version.C000vrfS.js";
 					sit, you're training your mind in some way, even if it doesn't feel like it. There's no
 					such thing as a bad meditation. Maybe you notice frustration, restlessness, or boredom -
 					that's insight. Maybe you notice tiny moments of focus - that's also insight. The key is
-					to keep practicing and stay curious about whatever shows up, with openness and kindness."</p> <p>It's also useful and important to keep practicing regularly - not just formal practice of
+					to keep practicing and stay curious about whatever shows up, with openness and kindness.</p> <p>It's also useful and important to keep practicing regularly - not just formal practice of
 					at least ten minutes per day, everyday, but also informal practice throughout each day,
 					such as mindfulness of posture, mindfulness of breathing in and out in all activities, or
 					mindfulness of daily activities. It's also useful to know that it may take a long time
