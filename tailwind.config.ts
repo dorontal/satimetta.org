76d474469a7ad947b341d449c7c7d6788390e15c
dorontal <mountainbot@gmail.com>
2025-04-24 13:10:47 -0400

    v1.0.32 - first version without Accordion, using HTML5 <details> / <summary> instead

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,47 +1,11 @@
 import type { Config } from 'tailwindcss';
 
-import { skeleton, contentPath } from '@skeletonlabs/skeleton/plugin';
-import * as themes from '@skeletonlabs/skeleton/themes';
-
 export default {
-  darkMode: 'selector',
-  content: [
-    './src/**/*.{html,js,svelte,ts}',
-    contentPath(import.meta.url, 'svelte')
-  ],
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [
-    skeleton({
-      // NOTE: each theme included will increase the size of your CSS bundle
-      themes: [
-        themes.cerberus
-        /* 
-        themes.catppuccin,
-        themes.cerberus,
-        themes.concord,
-        themes.crimson,
-        themes.fennec,
-        themes.hamlindigo,
-        themes.legacy,
-        themes.mint,
-        themes.modern,
-        themes.mona,
-        themes.nosh,
-        themes.nouveau,
-        themes.pine,
-        themes.reign,
-        themes.rocket, 
-        themes.rose,
-        themes.sahara,
-        themes.seafoam,
-        themes.terminus,
-        themes.vintage,
-        themes.vox,
-        themes.wintry 
-        */      
-      ]
-    })
-  ]
-} satisfies Config
+
+  plugins: []
+} satisfies Config;
