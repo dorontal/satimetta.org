ee164d2210ff97bacd7acad04131331a8e2f31d6
dorontal <mountainbot@gmail.com>
2025-05-31 09:22:45 -0400

    v1.0.51

--- src/routes/about-doron-tal/+page.svelte
+++ src/routes/about-doron-tal/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+	import TaijiQigong from './TaijiQigong.svelte';
+</script>
+
 <svelte:head>
 	<meta
 		name="description"
@@ -66,47 +70,14 @@
 				</dd>
 			</div>
 			<div>
-				<dt>Qigong (Chi Kung) (1989 - Present)</dt>
+				<dt>Yoga Practice (2010 - Present)</dt>
 				<dd>
-					Studied and practiced a 15-posture subset of the Shibashi Qigong (Set-1) 18-postures form
-					with
-					<a
-						class="anchor"
-						href="https://www.eomega.org/workshops/teachers/robert-tangora"
-						target="_blank"
-					>
-						Robert Tangora</a
-					>.
+					Regular pracctice of a Hatha yoga set similar to the one taught in the MBSR curriculum
+					(see the guided yoga videos by Dr. Lynn Rossi in the Resources section of this
+					website).
 				</dd>
 			</div>
-			<div>
-				<dt>Tai Chi (Taiji) (1988 - Present)</dt>
-				<dd>
-					Tai Chi is not only a martial art but also moving meditation. Studied and practiced the
-					<a class="anchor" href="https://www.youtube.com/watch?v=3_BKwlpOAkk" target="_blank">
-						Yang style 37-movement form</a
-					>
-					of
-					<a class="anchor" href="https://en.wikipedia.org/wiki/Cheng_Man-ch%27ing"
-						>Chen Man-ch'ing</a
-					>
-					with several teachers (in chronological order): Katherine Hannah,
-					<a class="anchor" href="https://www.youtube.com/watch?v=6rLG3Ys-wIU" target="_blank"
-						>Maggie Newman</a
-					>,
-					<a
-						class="anchor"
-						href="https://www.facebook.com/UCBTaiji/videos/william-dere-pushing-hands-principles/2454512608101776"
-						target="_blank">Sifu William Dere</a
-					>. Since 1991, studied with
-					<a class="anchor" href="https://www.williamccchen.com" target="_blank"
-						>Grandmaster William C. C. Chen</a
-					>, practicing his
-					<a class="anchor" href="https://www.youtube.com/watch?v=-MwaUnGM7lU" target="_blank"
-						>Yang style 60-movement form</a
-					> twice a day.
-				</dd>
-			</div>
+			<TaijiQigong />
 			<div>
 				<dt>Breathwork & Body Scans (Mid 1980's - Present)</dt>
 				<dd>
@@ -297,48 +268,7 @@
 					>. This catalyzed further study of his work, mostly in the area of loving-kindness.
 				</dd>
 			</div>
-			<div>
-				<dt>Qigong (Chi Kung) (1989 - Present)</dt>
-				<dd>
-					Studied and practiced a 15-posture subset of the Shibashi Qigong (Set-1) 18-postures form
-					with
-					<a
-						class="anchor"
-						href="https://www.eomega.org/workshops/teachers/robert-tangora"
-						target="_blank"
-					>
-						Robert Tangora</a
-					>.
-				</dd>
-			</div>
-			<div>
-				<dt>Tai Chi (Taiji) (1988 - Present)</dt>
-				<dd>
-					Tai Chi is not only a martial art but also moving meditation. Studied and practiced the
-					<a class="anchor" href="https://www.youtube.com/watch?v=3_BKwlpOAkk" target="_blank">
-						Yang style 37-movement form</a
-					>
-					of
-					<a class="anchor" href="https://en.wikipedia.org/wiki/Cheng_Man-ch%27ing"
-						>Chen Man-ch'ing</a
-					>
-					with several teachers (in chronological order): Katherine Hannah,
-					<a class="anchor" href="https://www.youtube.com/watch?v=6rLG3Ys-wIU" target="_blank"
-						>Maggie Newman</a
-					>,
-					<a
-						class="anchor"
-						href="https://www.facebook.com/UCBTaiji/videos/william-dere-pushing-hands-principles/2454512608101776"
-						target="_blank">Sifu William Dere</a
-					>. Since 1991, studied with
-					<a class="anchor" href="https://www.williamccchen.com" target="_blank"
-						>Grandmaster William C. C. Chen</a
-					>, practicing his
-					<a class="anchor" href="https://www.youtube.com/watch?v=-MwaUnGM7lU" target="_blank"
-						>Yang style 60-movement form</a
-					> twice a day.
-				</dd>
-			</div>
+			<TaijiQigong />
 		</dl>
 	</details>
 	<hr class="hr" />
---
+++ src/routes/about-doron-tal/TaijiQigong.svelte
@@ -0,0 +1,39 @@
+<div>
+	<dt>Qigong (Chi Kung) (1989 - Present)</dt>
+	<dd>
+		Studied and practiced a 15-posture subset of the Shibashi Qigong (Set-1) 18-postures form with
+		<a
+			class="anchor"
+			href="https://www.eomega.org/workshops/teachers/robert-tangora"
+			target="_blank"
+		>
+			Robert Tangora</a
+		>.
+	</dd>
+</div>
+<div>
+	<dt>Tai Chi (Taiji) (1988 - Present)</dt>
+	<dd>
+		Studied and practiced the
+		<a class="anchor" href="https://www.youtube.com/watch?v=3_BKwlpOAkk" target="_blank">
+			Yang style 37-movement form</a
+		>
+		of
+		<a class="anchor" href="https://en.wikipedia.org/wiki/Cheng_Man-ch%27ing">Chen Man-ch'ing</a>
+		with several teachers: Katherine Hannah,
+		<a class="anchor" href="https://www.youtube.com/watch?v=6rLG3Ys-wIU" target="_blank"
+			>Maggie Newman</a
+		>,
+		<a
+			class="anchor"
+			href="https://www.facebook.com/UCBTaiji/videos/william-dere-pushing-hands-principles/2454512608101776"
+			target="_blank">Sifu William Dere</a
+		>. Since 1991, studied with
+		<a class="anchor" href="https://www.williamccchen.com" target="_blank"
+			>Grandmaster William C. C. Chen</a
+		>, practicing his
+		<a class="anchor" href="https://www.youtube.com/watch?v=-MwaUnGM7lU" target="_blank"
+			>Yang style 60-movement form</a
+		> twice daily, as a moving meditation.
+	</dd>
+</div>
